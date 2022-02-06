5b3ff205d68a186b88219a57286ea7b4164a36b8
bd-webdesign <bd.webdesign99@gmail.com>
2022-02-06 20:20:01 +0200

    Header

--- client/layout/header.php
+++ client/layout/header.php
@@ -8,25 +8,44 @@
     <title>Nature Fairy Tales</title>
     <link rel="icon" type="image/png" href="/NFT/assets/img/logo/nft-modified.png" sizes="100x100">
     <link rel="stylesheet" href="/NFT/assets/css/layout.css?v=<?php echo time(); ?>">
+    <script src="https://kit.fontawesome.com/bf492b8f48.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
     <div class="nav">
-        <div class="logo">
-            <img src="/NFT/assets/img/logo/nft-modified.png" alt="logo">
-        </div>
-        <div class="text">
-            Nature Fairy Tales
-        </div>
-        <ul class="menu">
-            <li><a href="">About</a></li>
-            <li><a href="">Collection</a></li>
-            <li><a href="">Team</a></li>
-            <li><a href="">FAQs</a></li>
+        <!-- <div class="logo">
+                <img src="/NFT/assets/img/logo/nft-modified.png" alt="logo">
+            </div>
+            <div class="text">
+                Nature Fairy Tales
+            </div>
+            <ul class="menu">
+                <li><a href="">About</a></li>
+                <li><a href="">Collection</a></li>
+                <li><a href="">Team</a></li>
+                <li><a href="">FAQs</a></li>
+            </ul>
+            <ul class="social">
+                <li><a href="https://www.instagram.com/nftales2022/"><img src="/NFT/assets/img/icons/instagram.png" alt="instagram"></a></li>
+                <li><a href="https://twitter.com/nature_fairy22"><img src="/NFT/assets/img/icons/twitter.png" alt="twitter"></a></li>
+            </ul> -->
+        <ul>
+            <li class="logo"><img src="/NFT/assets/img/logo/nft-modified.png" alt="logo"></li>
+            <li class="text">Nature Fairy Tales</li>
         </ul>
-        <ul class="social">
-            <li><a href="https://www.instagram.com/nftales2022/"><img src="/NFT/assets/img/icons/instagram.png" alt="instagram"></a></li>
-            <li><a href="https://twitter.com/nature_fairy22"><img src="/NFT/assets/img/icons/twitter.png" alt="twitter"></a></li>
+        <ul>
+            <div class="menu">
+                <li><a href="">About</a></li>
+                <li><a href="">Collection</a></li>
+                <li><a href="">Team</a></li>
+                <li><a href="">FAQs</a></li>
+            </div>
+        </ul>
+        <ul>
+            <div class="social">
+                <li><a href="https://www.instagram.com/nftales2022/"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="https://twitter.com/nature_fairy22"><i class="fab fa-twitter"></i></a></li>
+            </div>
         </ul>
     </div>
 </body>
