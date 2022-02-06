5a551b3d8a6c9bba5fc62066bd10efa2539c432f
bd-webdesign <bd.webdesign99@gmail.com>
2022-02-06 13:14:18 +0200

    NFT Project

---
+++ client/layout/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nature Fairy Tales</title>
+    <link rel="icon" type="image/png" href="/NFT/assets/img/logo/nft-modified.png" sizes="100x100">
+    <link rel="stylesheet" href="/NFT/assets/css/layout.css?v=<?php echo time(); ?>">
+</head>
+
+<body>
+    <div class="nav">
+        <div class="logo">
+            <img src="/NFT/assets/img/logo/nft-modified.png" alt="logo">
+        </div>
+        <div class="text">
+            Nature Fairy Tales
+        </div>
+        <ul class="menu">
+            <li><a href="">About</a></li>
+            <li><a href="">Collection</a></li>
+            <li><a href="">Team</a></li>
+            <li><a href="">FAQs</a></li>
+        </ul>
+        <ul class="social">
+            <li><a href="https://www.instagram.com/nftales2022/"><img src="/NFT/assets/img/icons/instagram.png" alt="instagram"></a></li>
+            <li><a href="https://twitter.com/nature_fairy22"><img src="/NFT/assets/img/icons/twitter.png" alt="twitter"></a></li>
+        </ul>
+    </div>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,2 @@
+<?php
+include "client/layout/header.php";
