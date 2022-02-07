34085a720a6eabe14aca5c3ac8f1e7800196a2a5
bd-webdesign <bd.webdesign99@gmail.com>
2022-02-07 17:08:03 +0200

    ready header

---
+++ assets/js/js.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    var btn = $('#button');
+    var menu = $('#menu');
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 150) {
+            btn.addClass('show');
+            menu.css('height','55px');
+        } else {
+            btn.removeClass('show');
+            menu.css('height','65px');
+        }
+    });
+
+    btn.on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, '300');
+    });
+});
