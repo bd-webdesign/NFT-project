fb6d71453938ad7cb1900cdcd2f446f4fd9706b5
bd-webdesign <bd.webdesign99@gmail.com>
2022-02-08 23:35:45 +0200

    didi

--- client/collectionView.php
+++ client/collectionView.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/NFT/client/layout/header.php"; ?>
 <!-- <div style="height: 1000px;"></div> -->
 <section class="collection" id="collection">
@@ -8,12 +7,3 @@
 </section>
 
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/NFT/client/layout/footer.php"; ?>
-=======
-<?php include "layout/header.php"; ?>
-
-<div id="view-collection">
-
-</div>
-
-<?php include "layout/footer.php"; ?>
->>>>>>> 2306d130a5a8484ccad1358dcd3681f3656a0826
